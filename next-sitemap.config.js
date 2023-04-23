b4abb1d8b27f1bb0236cca90323e737b6be7e3d8
Jhagas <jhagas.205001@mhs.its.ac.id>
2023-04-23 18:06:02 +0700

    sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://jhagas.space",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
