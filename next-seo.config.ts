b28b1cf5e6127e4d1eb3b31eebe2539b2db8f572
Jhagas <jhagas.205001@mhs.its.ac.id>
2023-04-24 16:14:31 +0700

    seo optimization

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -3,11 +3,12 @@ import { DefaultSeoProps } from "next-seo";
 const config: DefaultSeoProps = {
   title: "Jhagas's Space, Science and Technology Blog",
   description:
-    "Exploring Jhagas's Space, a personal blog, and my journey through the fascinating world of science, physics, engineering, bleeding edge technologies and education advices",
+    "Exploring Jhagas's Space, a personal blog, and my journey through the fascinating world of science, physics, engineering and bleeding edge technologies",
+  canonical: "https://www.jhagas.space/",
   openGraph: {
     title: "Jhagas's Space, Science and Technology Blog",
     description:
-      "Exploring Jhagas's Space, a personal blog, and my journey through the fascinating world of science, physics, engineering, bleeding edge technologies and education advices",
+      "Exploring Jhagas's Space, a personal blog, and my journey through the fascinating world of science, physics, engineeringa and bleeding edge technologies",
     type: "website",
     locale: "id_ID",
     url: "https://www.jhagas.space/",
